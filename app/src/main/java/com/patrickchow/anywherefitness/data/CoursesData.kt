9fccf7e83c3d762802609bd65421a2a15a3b3c0d
Patrick Chow <PatrickChow0803@gmail.com>
2019-08-28 12:26:18 -0400

    Added data for courses

---
+++ app/src/main/java/com/patrickchow/anywherefitness/data/CoursesData.kt
@@ -0,0 +1,39 @@
+package com.patrickchow.anywherefitness.data
+
+import java.sql.Timestamp
+
+val courseId = arrayOf(
+    101,
+    102,
+    103,
+    104,
+    105,
+    106
+)
+
+val courseName = arrayOf(
+    "Yoga",
+    "Dance",
+    "Cycling",
+    "Strength Training",
+    "Pilates",
+    "Barre"
+)
+
+val instructor_id = arrayOf(
+    1,
+    2,
+    3,
+    4,
+    5,
+    6
+)
+
+val time = arrayOf(
+    Timestamp(System.currentTimeMillis()).time,
+    Timestamp(System.currentTimeMillis()).time,
+    Timestamp(System.currentTimeMillis()).time,
+    Timestamp(System.currentTimeMillis()).time,
+    Timestamp(System.currentTimeMillis()).time,
+    Timestamp(System.currentTimeMillis()).time
+)
