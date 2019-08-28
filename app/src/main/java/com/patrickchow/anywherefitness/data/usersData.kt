61b5021797890e16fd82b5f023262ea53b01c698
Patrick Chow <PatrickChow0803@gmail.com>
2019-08-28 12:44:03 -0400

    Added data for users

--- app/src/main/java/com/patrickchow/anywherefitness/data/usersData.kt
+++ app/src/main/java/com/patrickchow/anywherefitness/data/usersData.kt
@@ -1,2 +1,67 @@
 package com.patrickchow.anywherefitness.data
 
+/*
+Data for users
+data class UsersModel(
+    val firstName: String,  //first name of the user
+    val lastName: String,   //last name of the user
+    val userName: String,   //username of the user
+    var password: String,   //password of the user
+    val client: Boolean = false, //should be set to True if the user is a client
+    var instructor: Boolean = false //should be set to True if the user is an instructor
+)
+ */
+
+val firstName = arrayOf(
+    "Patrick",
+    "Benny",
+    "David",
+    "Grace",
+    "Amy",
+    "Kevin"
+)
+
+val secondName = arrayOf(
+    "Chow",
+    "Smith",
+    "Lee",
+    "Jones",
+    "Rakin",
+    "Lau"
+)
+
+val userName = arrayOf(
+    "PatrickChow0803",
+    "Machomacho2",
+    "DavidLifts111",
+    "Protein4Life",
+    "swolLyfe",
+    "KevinxD"
+)
+
+val passWord = arrayOf(
+    "abc123",
+    "password",
+    "111222",
+    "lovelovelove",
+    "drowssap",
+    "password123"
+)
+
+val client = arrayOf(
+    false,
+    false,
+    true,
+    true,
+    false,
+    true
+)
+
+val instructor = arrayOf(
+    true,
+    true,
+    false,
+    false,
+    true,
+    false
+)
