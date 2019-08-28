feed8c1c5501d9f891669e91bc90b3c3641fa053
Patrick Chow <PatrickChow0803@gmail.com>
2019-08-28 10:09:47 -0400

    Added a share button that allows the user to share a link to the official Anywhere Fitness website

--- app/src/main/java/com/patrickchow/anywherefitness/activities/MainActivity.kt
+++ app/src/main/java/com/patrickchow/anywherefitness/activities/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.patrickchow.anywherefitness.activities
 
 import android.content.Intent
+import android.media.Image
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
 import com.patrickchow.anywherefitness.R
 import com.patrickchow.anywherefitness.repositories.UsersRepository
 import kotlinx.android.synthetic.main.activity_main.*
@@ -15,6 +18,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         Log.i("${UsersRepository.usersList}", "Test")
+
+        //Allows the user to share the official Fitness Anywhere website
+        iv_share.setOnClickListener {
+            mainWebpageShare()
+        }
 
         //Intent to move to the list of trainer details
         btn_trainers.setOnClickListener {
@@ -34,6 +42,19 @@ class MainActivity : AppCompatActivity() {
             val loginIntent = Intent(this, LoginActivity::class.java)
             startActivity(loginIntent)
         }
+    }
 
+    //Gives the share button functionality to message someone a link to the official Anywhere Fitness website
+    fun mainWebpageShare(){
+        val webpageShareIntent = Intent()
+        webpageShareIntent.action = Intent.ACTION_SEND
+        webpageShareIntent.putExtra(Intent.EXTRA_TEXT,
+            """
+                https://anywherefitness-08-2019.netlify.com/
+                Fitness training can happen anywhere.
+                Book your classes today and train with the professionals!"
+            """.trimIndent())
+        webpageShareIntent.type = "text/plain"
+        startActivity(Intent.createChooser(webpageShareIntent, "Share with"))
     }
 }
